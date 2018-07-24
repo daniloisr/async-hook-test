2d2cc399a2a41968bf4e7f5cecc84155c20cd4aa
Danilo Resende <danilo.resende@toptal.com>
2018-07-24 12:36:55 -0300

    Remove comments and improve names

--- app.js
+++ app.js
@@ -2,58 +2,24 @@ const http = require('http')
 const { createHook, executionAsyncId } = require('async_hooks')
 const fs = require('fs')
 let ctx = new Map()
-function globalCtx() { return ctx }
 let currentCtx
 
-let indent = 0
-// createHook({
-//   init(id, type, triggerId) {
-//     if (type !== 'PROMISE') return
-//     const val = ctx.get(triggerId)
-//     ctx.set(id, val)
-//   },
-//   before(id) {
-//     const parentCtx = ctx.get(id)
-//     if (parentCtx) currentCtx = parentCtx
-//   },
-//   // after(id) {
-//   //   const parentCtx = ctx.get(id)
-//   //   if (parentCtx) currentCtx = parentCtx
-//   // },
-//   destroy(id) {
-//     ctx.delete(id)
-//   }
-// }).enable()
 createHook({
-  init(asyncId, type, triggerAsyncId) {
-    const eid = executionAsyncId();
-    const indentStr = ' '.repeat(indent);
-    fs.writeSync(
-      1,
-      `${indentStr}${type}(${asyncId}):` +
-      ` trigger: ${triggerAsyncId} execution: ${eid}\n`);
+  init(id, type, triggerId) {
     if (type !== 'PROMISE') return
-    const val = ctx.get(triggerAsyncId)
-    ctx.set(asyncId, val)
+    const val = ctx.get(triggerId)
+    ctx.set(id, val)
   },
-  before(asyncId) {
-    const indentStr = ' '.repeat(indent);
-    fs.writeSync(1, `${indentStr}before:  ${asyncId}\n`);
-    indent += 2;
-    const parentCtx = ctx.get(asyncId)
+  before(id) {
+    const parentCtx = ctx.get(id)
     if (parentCtx) currentCtx = parentCtx
   },
-  after(asyncId) {
-    indent -= 2;
-    const indentStr = ' '.repeat(indent);
-    fs.writeSync(1, `${indentStr}after:   ${asyncId}\n`);
-    const parentCtx = ctx.get(asyncId)
+  after(id) {
+    const parentCtx = ctx.get(id)
     if (parentCtx) currentCtx = parentCtx
   },
-  destroy(asyncId) {
-    const indentStr = ' '.repeat(indent);
-    fs.writeSync(1, `${indentStr}destroy: ${asyncId}\n`);
-    ctx.delete(asyncId)
+  destroy(id) {
+    ctx.delete(id)
   },
 }).enable();
 
@@ -70,8 +36,10 @@ http.createServer(function (req, res) {
   if (req.url === '/favicon.ico') return
 
   const id = executionAsyncId()
+  // TODO: set the ctx for the first time in a helper method?
   ctx.set(id, { val: id })
   currentCtx = { val: id }
+
   fs.writeSync(1, `---- ${currentCtx.val} started\n`)
   async().then(() => {
     fs.writeSync(1, `---- ${currentCtx.val} ended\n`)
