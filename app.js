c6235c90d69505cb48e62ce1cdbe15ce46a3f7f2
Danilo Resende <danilo.resende@toptal.com>
2018-07-24 13:16:33 -0300

    Simplify map usage

--- app.js
+++ app.js
@@ -2,21 +2,11 @@ const http = require('http')
 const { createHook, executionAsyncId } = require('async_hooks')
 const fs = require('fs')
 let ctx = new Map()
-let currentCtx
 
 createHook({
   init(id, type, triggerId) {
-    if (type !== 'PROMISE') return
-    const val = ctx.get(triggerId)
-    ctx.set(id, val)
-  },
-  before(id) {
-    const parentCtx = ctx.get(id)
-    if (parentCtx) currentCtx = parentCtx
-  },
-  after(id) {
-    const parentCtx = ctx.get(id)
-    if (parentCtx) currentCtx = parentCtx
+    if (type === 'PROMISE' && ctx.has(triggerId))
+      ctx.set(id, ctx.get(triggerId))
   },
   destroy(id) {
     ctx.delete(id)
@@ -37,12 +27,13 @@ http.createServer(function (req, res) {
 
   const id = executionAsyncId()
   // TODO: set the ctx for the first time in a helper method?
-  ctx.set(id, { val: id })
   currentCtx = { val: id }
+  ctx.set(id, currentCtx)
 
   fs.writeSync(1, `---- ${currentCtx.val} started\n`)
   async().then(() => {
+    currentCtx = ctx.get(executionAsyncId())
     fs.writeSync(1, `---- ${currentCtx.val} ended\n`)
     res.end('success')
   })
-}).listen(3000)
+}).listen(3001)
